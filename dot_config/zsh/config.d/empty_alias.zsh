f3765e5587e83cbb3459c3fb2944ae9d231ada5b
Ramil Aleshkin <alewkinr@gmail.com>
2023-08-18 00:47:49 +0400

     Substituted the aws from python to aws from brew

--- dot_config/zsh/config.d/empty_alias.zsh
+++ dot_config/zsh/config.d/empty_alias.zsh
@@ -12,10 +12,10 @@ alias kctx="kubectx"
 alias kns="kubens"
 
 ## AWS CLI
-alias aws="python3 $PYTHONPATH/aws"
-alias awsmts="python3 $PYTHONPATH/aws --profile oa-s3 --endpoint-url=https://s3mts.ru:443"
-alias awsgeomts="python3 $PYTHONPATH/aws --profile geo-s3 --endpoint-url=https://geo.s3mts.ru:443"
-alias aws_completer="python3 $PYTHONPATH/aws_completer"
+# alias aws="python3 $PYTHONPATH/aws" # fixed for brew binary
+alias awsmts="aws --profile oa-s3 --endpoint-url=https://s3mts.ru:443"
+alias awsgeomts="aws --profile geo-s3 --endpoint-url=https://geo.s3mts.ru:443"
+alias aws_completer="aws_completer"
 
 ## Python-tools
 alias notebook="$PYTHONPATH/jupyter notebook"
