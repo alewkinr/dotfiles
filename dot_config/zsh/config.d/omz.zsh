45ea4baa288c241a04be548345e8be5fc045acc5
Ramil Aleshkin <alewkinr@MacBook-Pro-M2.local>
2023-10-16 23:24:26 +0400

    :label: Typed git submodules directory

--- dot_config/zsh/config.d/omz.zsh
+++ dot_config/zsh/config.d/omz.zsh
@@ -1,7 +1,7 @@
 ### OH-MY-ZSH.SH ###
 
 # ZSH defines the path to your [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh) installation
-export ZSH="$(chmz source-path)/include/ohmyzsh"
+export ZSH="$(chmz source-path)/external_include/ohmyzsh"
 
 # Source plugins installed with brew. Copy to omz directory is not required.
 source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" # https://github.com/zsh-users/zsh-autosuggestions
