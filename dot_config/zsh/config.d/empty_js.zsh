7bed660f76e2e332cd518eb71035521ab127ebb3
Ramil Aleshkin <alewkinr@gmail.com>
2023-10-22 12:51:52 +0400

    :sparkles: Added managed npm dotfile

--- dot_config/zsh/config.d/empty_js.zsh
+++ dot_config/zsh/config.d/empty_js.zsh
@@ -1,4 +1,8 @@
 ### JavaScript / TypeScript ###
 
+# NPM_GLOBAL_PATH defines path variable for global [npm](https://docs.npmjs.com)
+# For npm global path should be added in PATH since it is installed via homebrew
+# If not, try `cd "$(npm -g prefix)/bin"`
+
 # YARNGLOBALPATH defines path variable to gloabal [Yarn](https://github.com/yarnpkg/berry)
 export YARNGLOBALPATH="$XDG_CONFIG_HOME/yarn/global"
