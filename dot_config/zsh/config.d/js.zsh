fc611863d3bd21da9948de616e26471583db674d
Ramil Aleshkin <alewkinr@gmail.com>
2023-10-26 17:33:19 +0400

    :sparkles: Added ~/.config/npm to managed
    - fixed path to cache directory

--- dot_config/zsh/config.d/js.zsh
+++ dot_config/zsh/config.d/js.zsh
@@ -4,5 +4,8 @@
 # For npm global path should be added in PATH since it is installed via homebrew
 # If not, try `cd "$(npm -g prefix)/bin"`
 
+# NPM_CONFIG_USERCONFIG defines path to userconfig for npm
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/userconfig"
+
 # YARNGLOBALPATH defines path variable to gloabal [Yarn](https://github.com/yarnpkg/berry)
 export YARNGLOBALPATH="$XDG_CONFIG_HOME/yarn/global"
